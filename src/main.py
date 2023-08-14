7ee5beec4a8f1a4486675b1516019a1623c80aeb
Denis Komarov <d.komarov@redmadrobot.com>
2023-08-14 16:53:11 +0400

    movie add functionality

--- src/main.py
+++ src/main.py
@@ -1,12 +1,42 @@
-from fastapi import FastAPI
+from typing import Union
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+class Movie(BaseModel):
+    name: str
+
 
 app = FastAPI()
 
+db = {
+    0: "Lord of the Rings",
+}
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/extra")
 async def root():
     return {"message": "Hello World Extra"}
+
+
+@app.get("/movies/{movie_id}")
+async def get_movie(movie_id: int) -> str:
+    movie = db.get(movie_id, None)
+    
+    if movie is None:
+        raise HTTPException(status_code=404, detail=f"Movie {movie_id} not found")
+    
+    return movie
+
+
+@app.post("/movies/{movie_id}")
+async def add_movie(movie_id:int, movie: Movie):
+    if movie_id in db.keys():
+        raise HTTPException(status_code=400, detail=f"Bad Request Error. Movie with id: {movie_id} already exists.")
+    
+    db[movie_id] = movie.name
